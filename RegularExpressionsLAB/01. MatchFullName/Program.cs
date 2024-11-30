d8229acccadbe02a74a30bd71d3ed85ffaad3d4c
Kreizy276 <krisbg02@gmail.com>
2024-11-30 20:28:14 +0200

    regular expressions lab

---
+++ RegularExpressionsLAB/01. MatchFullName/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patter = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)";
+            string input = Console.ReadLine();
+
+            MatchCollection result = Regex.Matches(input, patter);
+
+            foreach (Match match in result)
+            {
+                Console.Write($"{match} ");
+            }
+        }
+    }
+}
---
+++ RegularExpressionsLAB/02. MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<=^|\s)\+359 ((\d{1} \d{3} \d{4})|(\d{1}-\d{3}-\d{4}))\b";
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            var matchedPhones = matches.Cast<Match>().Select(match => match.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
